c55a6f6602d33a29036fc8a02874cd3ca36facc9
ArifAKareem <57692115+ArifAKareem@users.noreply.github.com>
2022-02-09 22:25:27 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_chat import message
 
-st.text_input('this')
+msg = st.text_input('this')
 message("My message") 
-message("Hello bot!", is_user=True)  # align's the message to the right
+message(msg, is_user=True)  # align's the message to the right
