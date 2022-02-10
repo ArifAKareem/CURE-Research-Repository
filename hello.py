b38aaf2cb1f57b5760c804b23080923abb39da51
ArifAKareem <57692115+ArifAKareem@users.noreply.github.com>
2022-02-09 22:31:13 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,8 +7,8 @@ st.set_page_config(
     page_icon=":robot:"
 )
 
-API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
-headers = {"Authorization": st.secrets['api_key']}
+#API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
+#headers = {"Authorization": st.secrets['api_key']}
 
 st.header("Streamlit Chat - Demo")
 st.markdown("[Github](https://github.com/ai-yash/st-chat)")
@@ -20,8 +20,8 @@ if 'past' not in st.session_state:
     st.session_state['past'] = []
 
 def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
+	#esponse = requests.post(API_URL, headers=headers, json=payload)
+	return 'hello'
 
 def get_text():
     input_text = st.text_input("You: ","Hello, how are you?", key="input")
